980339d78f55dfc723ff5c3cab5393539501ad79
Christopher Woodhouse <woodhousecas@gmail.com>
2020-04-03 00:59:59 +0100

    ok

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import random
 
 # Initialise the pygame #
@@ -30,15 +31,23 @@ def player(x, y):
     screen.blit(playerImg, (x, y)) # Blit means to draw
 
 # Enemy Target
+EnemyImg = []
+EnemyX = []
+EnemyY = []
+EnemyX_Change = []
+EnemyY_Change = []
+num_of_enemies = 6
 
-enemyImg = pygame.image.load('Alien.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_Change = 3
-enemyY_Change = 40
+for i in range(num_of_enemies):
+    EnemyImg.append(pygame.image.load('Alien.png'))
+    EnemyX.append(random.randint(0, 735))
+    EnemyY.append(random.randint(50, 150))
+    EnemyX_Change.append(2)
+    EnemyY_Change.append(40)
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y)) # Blit means to draw
+
+def enemy(x, y, i):
+    screen.blit(EnemyImg[i], (x, y))
 
 # Bullet # Ready state means can't see the bullet on the screen, its ready to be fired. # Fire means the bullet is currently moving.
 BulletImg = pygame.image.load('Bullet.png')
@@ -52,6 +61,20 @@ def Fire_Bullet(x, y):
     global Bullet_State
     Bullet_State = "Fire"
     screen.blit(BulletImg, (x + 16, y + 10))
+
+# Concept of Collision Detection
+# We take away the distance between bullet and target, and If the distance is short enough, we say it has hit.
+
+def isCollision(EnemyX, EnemyY, BulletX, BulletY):
+    Distance = math.sqrt((math.pow(EnemyX[i] - BulletX, 2)) + (math.pow(EnemyY[i] - BulletY, 2)))
+    if Distance < 27:
+        return True
+    else:
+        return False
+
+
+score = 0
+
 
 # Keeps the window open / Game Loop (Anything that must appear continuously in the game goes inside the game loop)
 running = True
@@ -95,16 +118,26 @@ while running:
         PlayerX = 0
     elif PlayerX >= 736:
         PlayerX = 736
-
-    enemyX += enemyX_Change
+    for i in range(num_of_enemies):
+        EnemyX[i] += EnemyX_Change[i]
 # Checking Boundaries Enemy
-    if enemyX <= 0:
-        enemyX_Change = 4
-        enemyY += enemyY_Change
-    elif enemyX >= 736:
-        enemyX_Change = -4
-        enemyY += enemyY_Change
+        if EnemyX[i] <= 0:
+            EnemyX_Change[i] = 4
+            EnemyY[i] += EnemyY_Change[i]
+        elif EnemyX[i] >= 736:
+            EnemyX_Change[i] = -4
+            EnemyY[i] += EnemyY_Change[i]
 
+        # Collision True or False
+        collision = isCollision(EnemyX, EnemyY, BulletX, BulletY)
+        if collision:
+            BulletY = 480
+            Bullet_State = "Ready"
+            score += 1
+            print(score)
+            EnemyX = random.randint(0, 735)
+            EnemyY = random.randint(50, 150)
+        enemy(EnemyX[i], EnemyY[i], i)
     # Bullet Movement
     if BulletY <= 0:
         BulletY = 480
@@ -114,8 +147,8 @@ while running:
         Fire_Bullet(BulletX, BulletY)
         BulletY -= BulletY_Change
 
+
     player(PlayerX, PlayerY)
-    enemy(enemyX, enemyY)
     pygame.display.update()
 # An event is anything that happens inside the game window or screen.
 
